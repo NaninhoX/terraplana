816412c11fb543b7928aa67c47243ce46608e1b8
Michel E A Ramon <125842179+NaninhoX@users.noreply.github.com>
2026-05-15 19:50:06 -0300

    Create constants.js

---
+++ js/utils/constants.js
@@ -0,0 +1,8 @@
+export const DR = 100;          // disc radius
+export const AR = 3;            // arctic inner radius
+export const R_CANCER = 38;     // Tropic of Cancer radius
+export const R_EQUATOR = 55;    // Equator radius
+export const R_CAPRI = 72;      // Tropic of Capricorn radius
+export const SUN_H = 28;        // sun height above disc
+export const MOON_H_BASE = 28;  // moon base height
+export const MOON_H_VARIATION = 12; // moon height variation
