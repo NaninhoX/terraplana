87903cd30d4de1ae24a5bba1f536c095c989798c
Michel E A Ramon <125842179+NaninhoX@users.noreply.github.com>
2026-05-15 21:14:24 -0300

    Fix renderer to use window properties for size

--- js/core/renderer.js
+++ js/core/renderer.js
@@ -2,8 +2,8 @@ import * as THREE from 'three';
 
 export function createRenderer() {
     const renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setPixelRatio(devicePixelRatio);
-    renderer.setSize(innerWidth, innerHeight);
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.shadowMap.enabled = true;
     renderer.shadowMap.type = THREE.PCFSoftShadowMap;
     return renderer;
