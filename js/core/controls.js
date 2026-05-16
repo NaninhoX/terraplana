f1a8fa6644a583bb803d5e8c400955d175231c69
Michel E A Ramon <125842179+NaninhoX@users.noreply.github.com>
2026-05-15 21:35:30 -0300

    Remove unused import of OrbitControls
    Removed import statement for OrbitControls.

--- js/core/controls.js
+++ js/core/controls.js
@@ -1,5 +1,3 @@
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-
 export function createOrbitControls(camera, domElement) {
     const controls = new OrbitControls(camera, domElement);
     controls.enableDamping = true;
